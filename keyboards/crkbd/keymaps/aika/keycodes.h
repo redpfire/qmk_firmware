9622cf32502c6f5f0e10e101675a051ed329f79d
redpfire <redpixafire@gmail.com>
2022-08-01 20:24:08 +0200

    refactor(crkbd): Move custom keycodes to a new file

---
+++ keyboards/crkbd/keymaps/aika/keycodes.h
@@ -0,0 +1,23 @@
+// Copyright 2022 Piotr Szczurkowski (@redpfire)
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#define LALTA LALT_T(KC_A)
+#define LALTO LALT_T(KC_O)
+#define LALTMPRV LALT_T(KC_MPRV)
+
+#define RALTR RALT_T(KC_R)
+#define RALTI RALT_T(KC_I)
+
+#define LSFTS LSFT_T(KC_S)
+#define LSFTE LSFT_T(KC_E)
+#define LSFTMSTP LSFT_T(KC_MSTP)
+#define LSFT5 LSFT_T(KC_5)
+
+#define LCTLT LCTL_T(KC_T)
+#define LCTLN LCTL_T(KC_N)
+#define LCTLMNXT LCTL_T(KC_MNXT)
+#define LCTL6 LCTL_T(KC_6)
+
+#define GUI_ESC LGUI_T(KC_ESC)
+#define L2_SPC LT(2, KC_SPC)
--- keyboards/crkbd/keymaps/aika/keymap.c
+++ keyboards/crkbd/keymaps/aika/keymap.c
@@ -2,26 +2,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include QMK_KEYBOARD_H
 #include "aika.h"
-
-#define LALTA LALT_T(KC_A)
-#define LALTO LALT_T(KC_O)
-#define LALTMPRV LALT_T(KC_MPRV)
-
-#define RALTR RALT_T(KC_R)
-#define RALTI RALT_T(KC_I)
-
-#define LSFTS LSFT_T(KC_S)
-#define LSFTE LSFT_T(KC_E)
-#define LSFTMSTP LSFT_T(KC_MSTP)
-#define LSFT5 LSFT_T(KC_5)
-
-#define LCTLT LCTL_T(KC_T)
-#define LCTLN LCTL_T(KC_N)
-#define LCTLMNXT LCTL_T(KC_MNXT)
-#define LCTL6 LCTL_T(KC_6)
-
-#define GUI_ESC LGUI_T(KC_ESC)
-#define L2_SPC LT(2, KC_SPC)
+#include "keycodes.h"
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
